219cfce6c8b1a4cf733d5946c67ff0bcbb7f4974
Ryan Spears <sperj001@hotmail.com>
2015-06-04 10:48:04 +0930

    Added schedule day directive

--- script.js
+++ script.js
@@ -62,13 +62,21 @@ app.controller("dragDropController", function ($scope) {
         }
     }
 
-    $scope.hoursInDay = ['1', '2','3','4','5','6','7','8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '23']
+    $scope.hoursInDay = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM']
 });
 
-app.directive('schedule', function () {
+app.directive('scheduleDay', function () {
     return {
         restrict: 'E',
         replace: true,
-        templateUrl:  'schedule.html'
+        templateUrl:  'schedule-day.html'
+    };
+});
+
+app.directive('scheduleHours', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl:  'schedule-hours.html'
     };
 });
